d8bd19969c1034c73574f0eed14298db4d5c1628
AlondraMayen <asmayen@uamv.edu.ni>
2025-05-20 11:26:44 -0600

    ejercicio1

---
+++ sesion1/ejercicio1.py
@@ -0,0 +1,5 @@
+"""Cargar una lista manualmente
+Crea una lista con 10 números enteros y muestra el contenido"""
+
+array_int = [1,2,3,4,5,6,7,8,9,10]
+print("Lista de números enteros: ", array_int)
