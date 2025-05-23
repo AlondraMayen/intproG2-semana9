ddc16fab3b05c3ad9c1c130a80b84bc8fa8abac0
AlondraMayen <asmayen@uamv.edu.ni>
2025-05-23 08:02:48 -0600

    ejercicio1

---
+++ sesion2/ejercicio1.py
@@ -0,0 +1,15 @@
+"""1. Registro de calificaciones
+Un docente desea registrar las calificaciones de sus 10 estudiantes en un examen.
+El programa debe:
+• Solicitar las calificaciones (sólo se permiten entre 0 a 100).
+• Mostrar el promedio, la calificación más alta, la más baja y cuántos aprobaron (>=
+70)."""
+
+estudiantes_int=[1,2,3,4,5,6,7,8,9,10]
+estudiante = 1
+
+for estudiante in estudiantes_int:
+    print ("Ingrese la calificación (0-100):, {estudiante}")
+    estudiante += 1
+    estudiantes_int 
+    
