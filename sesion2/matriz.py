08128178a7c3f09af1fa32daa06192c48d33a0ff
AlondraMayen <asmayen@uamv.edu.ni>
2025-05-23 08:04:57 -0600

    matriz

---
+++ sesion2/matriz.py
@@ -0,0 +1,6 @@
+# MATRIZ: es una tabla, esta compuesta por filas y columnas y estas conforman una celda
+matriz = [[1,2], [3,4]]
+for fila in matriz:
+    for columna in fila:
+        print(columna, end = " ")
+    print()
