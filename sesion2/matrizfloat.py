7734ffe25ba2e083d344907c1ef92b8303b00ce4
AlondraMayen <asmayen@uamv.edu.ni>
2025-05-23 08:06:34 -0600

    matriz_flotante

---
+++ sesion2/matrizfloat.py
@@ -0,0 +1,8 @@
+# MATRIZ FLOTANTE
+matriz = [[1.2, 2.3],[3.4, 4.5]]
+print("-"*17)
+for fila in matriz:
+    for columna in fila:
+        print(f"|{columna:>6.1f}", end = " ")
+    print("|")
+    print("-"*17)
